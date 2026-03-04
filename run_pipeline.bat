f7d41d277311cf930a1068fe3ba963076fad155e
BhavyaMehra <105857676+BhavyaMehra@users.noreply.github.com>
2026-03-04 17:45:36 +0530

    Added run_pipeline for scheduler

---
+++ run_pipeline.bat
@@ -0,0 +1,6 @@
+@echo off
+echo === Credit EWS Pipeline Starting ===
+
+C:\Users\BM\OneDrive\Desktop\Python_projects\Credit_Risk_Projects\credit-ews\venv\Scripts\python.exe C:\Users\BM\OneDrive\Desktop\Python_projects\Credit_Risk_Projects\credit-ews\src\pipeline.py
+
+echo === Pipeline Complete ===
